315f162e0fb79fc04a75d8d1a19ce845c38125a5
Shawn LoPorto <sloporto@asi-test.com>
2024-07-25 16:10:41 -0600

    More code

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,3 @@
-show_expected_result = False
-show_hints = False
 def is_palindrome(teststr):  
     lowerCaseWord=str.lower(teststr)
     newLowerCaseWord=''.join(filter(str.isalnum, lowerCaseWord))
@@ -19,11 +17,7 @@ def is_palindrome(teststr):
     print("The word ",teststr," is a palindrome.")
     return True
 
-
 #palindrome
-#The code will count the number of correct answers
-total = 0
 test_words = ["Hello World!","Radar","Mama?","Madam, I'm Adam.","Race car!"]
 for word in test_words:
-    # total += Answer.is_palindrome(word)
     print(is_palindrome(word))
