ad53e4590675420ad997157fa71e968726d00281
Shawn LoPorto <sloporto@asi-test.com>
2024-07-26 12:28:18 -0600

    More updates

--- scratchArea.py
+++ scratchArea.py
@@ -1,4 +1,36 @@
+#######################################################################################################
+#Bubble sort - swapping improvement to exit after encountering one run with no swapping, so we're done
+#######################################################################################################
+my_array = [7, 3, 9, 12, 11]
+
+n = len(my_array)
+for i in range(n-1):
+    swapped = False
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+            swapped = True
+    if not swapped:
+        break
+
+print("Sorted array:", my_array)
+
+##################################
+#Bubble sort
+##################################
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+
+print("Sorted array:", my_array)
+
+#################################################################################
 #Creating a basic Fibonnaci where you add the previous number to the next number
+#################################################################################
 print(0)
 print(1)
 count = 2
@@ -17,7 +49,9 @@ def fibonacci(prev1, prev2):
 
 fibonacci(1,0)
 
+##################################
 #Find the mininum value in a List
+##################################
 my_array = [7, 12, 9, 4, 11]
 minVal = my_array[0]
 
