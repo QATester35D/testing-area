48b88508a133aa6db01079cedde61231aac75cb0
Shawn LoPorto <sloporto@asi-test.com>
2024-07-25 20:33:33 -0600

    Added more logic

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,9 @@
-def is_palindrome(teststr):  
+#################################################################
+# Multiple ways to check if a word is a palindrome
+#################################################################
+# Version 1 - strip out space and compare first and last, working inward
+#################################################################
+def isThisAPalindrome(teststr):  
     lowerCaseWord=str.lower(teststr)
     newLowerCaseWord=''.join(filter(str.isalnum, lowerCaseWord))
     wordLength=len(newLowerCaseWord)-1
@@ -17,7 +22,33 @@ def is_palindrome(teststr):
     print("The word ",teststr," is a palindrome.")
     return True
 
-#palindrome
 test_words = ["Hello World!","Radar","Mama?","Madam, I'm Adam.","Race car!"]
 for word in test_words:
-    print(is_palindrome(word))
+    print(isThisAPalindrome(word))
+
+############################################################################################
+# Version 2 - using the reverse approach and work backwards using the string[index] operator
+############################################################################################
+def isPalindrome(string): 
+    if (string == string[::-1]):
+        return "The string is a palindrome." 
+    else: 
+        return "The string is not a palindrome." 
+ 
+#Enter input string 
+string = input ("Enter string: ")
+print(isPalindrome(string))
+
+#################################################################
+# Version 2 - using the reversed and join methods
+#################################################################
+def isPalindrome(string):
+    revstr=''.join(reversed(string))
+    if string==revstr:
+        return "The string is a palindrome."
+    else:
+        return "The string is not a palindrome."
+ 
+#Enter input string
+string = input ("Enter string: ")
+print(isPalindrome(string))
